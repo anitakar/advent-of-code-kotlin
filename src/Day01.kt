74707c8dad94d35e73dd5a5173f58208134c0657
Anita Fronczak <akarbow@gmail.com>
2022-12-11 20:39:08 +0100

    Day 1: Non-idiomatic first day solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,46 @@
+import java.util.PriorityQueue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0;
+        var maxSum = 0;
+        for (cur in input) {
+            if (cur.isEmpty()) {
+                if (sum > maxSum) {
+                    maxSum = sum
+                }
+                sum = 0
+            } else {
+                sum += cur.toInt()
+            }
+        }
+        return maxSum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val queue = PriorityQueue<Int>()
+        var sum = 0
+        for (elem in input) {
+            if (elem.isBlank()) {
+                if (queue.size >= 3) {
+                    if (queue.peek() < sum) {
+                        queue.poll()
+                        queue.add(sum)
+                    }
+                } else {
+                    queue.add(sum)
+                }
+                sum = 0;
+            } else {
+                sum += elem.toInt()
+            }
+        }
+        return queue.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     part1(input).println()
