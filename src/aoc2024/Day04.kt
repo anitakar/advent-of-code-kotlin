3f02d2fd966e2401ac4053e6d5f48084089e1c4a
Anita Fronczak <akarbow@gmail.com>
2024-12-04 09:08:26 +0100

    AoC 4--

--- src/aoc2024/Day04.kt
+++ src/aoc2024/Day04.kt
@@ -31,25 +31,41 @@ fun main() {
             result += findAll(toCheck)
             result += findAll(toCheck.reversed())
         }
-        for (column in 0 until input[0].length) {
-            for (row in 0 until input.size) {
-                var toCheck = ""
-                for (i in 0 until min(input[0].length - column, input.size - row)) {
-                    toCheck += input[row+i][column+i]
-                }
-                result += findAll(toCheck)
-                result += findAll(toCheck.reversed())
+        var column = 0
+        for (row in 0 until input.size) {
+            var toCheck = ""
+            for (i in 0 until min(input[0].length - column, input.size - row)) {
+                toCheck += input[row + i][column + i]
             }
+            result += findAll(toCheck)
+            result += findAll(toCheck.reversed())
         }
-        for (column in 0 until input[0].length) {
-            for (row in 0 until input.size) {
-                var toCheck = ""
-                for (i in 0 .. min(column, row)) {
-                    toCheck += input[row-i][column-i]
-                }
-                result += findAll(toCheck)
-                result += findAll(toCheck.reversed())
+        var row = 0
+        for (column in 1 until input[0].length) {
+            var toCheck = ""
+            for (i in 0 until min(input[0].length - column, input.size - row)) {
+                toCheck += input[row + i][column + i]
             }
+            result += findAll(toCheck)
+            result += findAll(toCheck.reversed())
+        }
+        column = input[0].length - 1
+        for (row in 0 until input.size) {
+            var toCheck = ""
+            for (i in 0..min(column, row)) {
+                toCheck += input[row - i][column - i]
+            }
+            result += findAll(toCheck)
+            result += findAll(toCheck.reversed())
+        }
+        row = input.size - 1
+        for (column in 0 until input[0].length - 1) {
+            var toCheck = ""
+            for (i in 0..min(column, row)) {
+                toCheck += input[row - i][column - i]
+            }
+            result += findAll(toCheck)
+            result += findAll(toCheck.reversed())
         }
         return result
     }
