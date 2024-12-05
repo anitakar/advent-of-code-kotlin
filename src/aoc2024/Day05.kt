f0f446195df887621b39b06b3c944439ab29d645
Anita Fronczak <afronczak@netflix.com>
2024-12-05 12:06:39 +0100

    AoC 5++

--- src/aoc2024/Day05.kt
+++ src/aoc2024/Day05.kt
@@ -32,18 +32,51 @@ fun main() {
         return input.subList(blankIndex + 1, input.size)
     }
 
-    fun validUpdateMiddleValue(line: String): Int {
-        val update = line.split(",").map { it.toInt() }
+    fun isValidUpdate(update: List<Int>): Boolean {
         val added = mutableSetOf<Int>()
 
         for (elem in update) {
             added.add(elem)
             if (beforeRules.contains(elem) && beforeRules[elem]!!.intersect(added).isNotEmpty()) {
-                return 0
+                return false
             }
         }
 
-        return update[update.size / 2]
+        return true
+    }
+
+    fun reorder(update: List<Int>): List<Int> {
+        val updated = mutableListOf<Int>()
+        updated.addAll(update)
+        val added = mutableSetOf<Int>()
+
+        for (elem in update) {
+            added.add(elem)
+            if (beforeRules.contains(elem) && beforeRules[elem]!!.intersect(added).isNotEmpty()) {
+                updated.remove(elem)
+                for (i in updated.indices) {
+                    if (beforeRules[elem]!!.contains(updated[i])) {
+                        updated.add(i, elem)
+                        break
+                    }
+                }
+            }
+        }
+
+        return updated
+    }
+
+    fun parseUpdate(line: String): List<Int> {
+        return line.split(",").map { it.toInt() }
+    }
+
+    fun validUpdateMiddleValue(line: String): Int {
+        val update = parseUpdate(line)
+        return if (isValidUpdate(update)) {
+            update[update.size / 2]
+        } else {
+            0
+        }
     }
 
     fun part1(input: List<String>): Long {
@@ -57,11 +90,19 @@ fun main() {
 
     fun part2(input: List<String>): Long {
         var result = 0L
+        val updates = translateInput(input)
+        for (update in updates) {
+            val updateParsed = parseUpdate(update)
+            if (!isValidUpdate(updateParsed)) {
+                val reordered = reorder(updateParsed)
+                result += reordered[reordered.size / 2]
+            }
+        }
         return result
     }
 
     println(part1(readInput("aoc2024/Day05_test")))
     println(part1(readInput("aoc2024/Day05")))
-//    println(part2(readInput("aoc2024/Day05_test")))
-//    println(part2(readInput("aoc2024/Day05")))
+    println(part2(readInput("aoc2024/Day05_test")))
+    println(part2(readInput("aoc2024/Day05")))
 }
