90c6e151a12d3e0eef5eee72c760b1f6a4d211f2
Anita Fronczak <akarbow@gmail.com>
2024-12-08 10:58:36 +0100

    AoC24 8++

--- src/aoc2024/Day08.kt
+++ src/aoc2024/Day08.kt
@@ -56,13 +56,58 @@ fun main() {
         return uniqueAntinodes.size
     }
 
-    fun part2(input: List<String>): Long {
-        var total = 0L
-        return total
+    fun part2(input: List<String>): Int {
+        val nodesLocations = mutableMapOf<Char, MutableList<Position>>()
+        for (i in input.indices) {
+            for (j in input[i].indices) {
+                if (input[i][j] == '.')
+                    continue
+                if (!nodesLocations.contains(input[i][j])) {
+                    nodesLocations[input[i][j]] = mutableListOf()
+                }
+                nodesLocations[input[i][j]]!!.add(Position(i, j))
+            }
+        }
+
+        val uniqueAntinodes = mutableSetOf<Position>()
+        uniqueAntinodes.addAll(nodesLocations.filter { it.value.size > 1 }.values.flatten())
+        for (node in nodesLocations) {
+            for (i in node.value.indices) {
+                for (j in (i + 1) until node.value.size) {
+                    val first = node.value[i]
+                    val second = node.value[j]
+
+                    var firstAntinode = Position(
+                        first.x - (second.x - first.x),
+                        first.y - (second.y - first.y)
+                    )
+                    while (withinBounds(firstAntinode.x, firstAntinode.y, input)) {
+                        uniqueAntinodes.add(firstAntinode)
+                        firstAntinode = Position(
+                            firstAntinode.x - (second.x - first.x),
+                            firstAntinode.y - (second.y - first.y)
+                        )
+                    }
+
+                    var secondAntinode = Position(
+                        second.x + (second.x - first.x),
+                        second.y + (second.y - first.y)
+                    )
+                    while (withinBounds(secondAntinode.x, secondAntinode.y, input)) {
+                        uniqueAntinodes.add(secondAntinode)
+                        secondAntinode = Position(
+                            secondAntinode.x + (second.x - first.x),
+                            secondAntinode.y + (second.y - first.y)
+                        )
+                    }
+                }
+            }
+        }
+        return uniqueAntinodes.size
     }
 
     println(part1(readInput("aoc2024/Day08_test")))
     println(part1(readInput("aoc2024/Day08")))
-//    println(part2(readInput("aoc2024/Day08_test")))
-//    println(part2(readInput("aoc2024/Day08")))
+    println(part2(readInput("aoc2024/Day08_test")))
+    println(part2(readInput("aoc2024/Day08")))
 }
