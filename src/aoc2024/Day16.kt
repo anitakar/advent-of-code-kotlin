ea9e38ffa337a39f7668b1b28f32495f455a561a
Anita Fronczak <afronczak@netflix.com>
2024-12-17 00:42:13 +0100

    AoC24 16++

--- src/aoc2024/Day16.kt
+++ src/aoc2024/Day16.kt
@@ -28,21 +28,34 @@ fun main() {
             return if (next != null && map.get(next) != '#') {
                 Pair(next, 'S')
             } else null
-        }  else {
+        } else {
             null
         }
     }
 
     val smallest = mutableMapOf<Pair<Position, Char>, Long>()
+    val smallestPrev = mutableMapOf<Pair<Position, Char>, MutableList<Pair<Position, Char>>>()
     val visited = mutableSetOf<Pair<Position, Char>>()
     val toVisit = PriorityQueue<Pair<Pair<Position, Char>, Long>> { a, b -> (a.second - b.second).toInt() }
 
-    fun updateCost(next: Pair<Position, Char>?, newCost: Long) {
+    fun updateCost(next: Pair<Position, Char>?, newCost: Long, prev: Pair<Position, Char>) {
         if (next != null) {
+            if (!smallestPrev.contains(next)) {
+                smallestPrev[next] = mutableListOf()
+            }
+
             if (smallest.contains(next)) {
+                val prevCost = smallest[next]!!
                 smallest[next] = min(smallest[next]!!, newCost)
+                if (newCost == smallest[next]) {
+                    if (newCost != prevCost) {
+                        smallestPrev[next]!!.clear()
+                    }
+                    smallestPrev[next]!!.add(prev)
+                }
             } else {
                 smallest[next] = newCost
+                smallestPrev[next]!!.add(prev)
             }
 
             if (!visited.contains(next)) {
@@ -55,6 +68,7 @@ fun main() {
         smallest.clear()
         visited.clear()
         toVisit.clear()
+        smallestPrev.clear()
 
         val map = Grid(input)
         val start = map.find('S')!!
@@ -62,7 +76,7 @@ fun main() {
 
         toVisit.add(Pair(Pair(start, 'E'), 0))
         smallest[Pair(start, 'E')] = 0
-        while(toVisit.isNotEmpty()) {
+        while (toVisit.isNotEmpty()) {
             val current = toVisit.poll()
             if (visited.contains(current.first))
                 continue
@@ -71,25 +85,41 @@ fun main() {
             val cost = current.second
 
             val nextStraight = nextStraight(current.first, map)
-            updateCost(nextStraight, cost + 1)
+            updateCost(nextStraight, cost + 1, current.first)
 
             val nextClockwise = Pair(current.first.first, Direction(current.first.second).nextClockwise()!!)
-            updateCost(nextClockwise, cost + 1000)
+            updateCost(nextClockwise, cost + 1000, current.first)
 
-            val nextCounterClockwise = Pair(current.first.first, Direction(current.first.second).nextCounterClockwise()!!)
-            updateCost(nextCounterClockwise, cost + 1000)
+            val nextCounterClockwise =
+                Pair(current.first.first, Direction(current.first.second).nextCounterClockwise()!!)
+            updateCost(nextCounterClockwise, cost + 1000, current.first)
         }
 
         return smallest.filter { it.key.first == end }.minBy { it.value }?.value
     }
 
-    fun part2(input: List<String>): Long {
-        return 0L
+    fun part2(input: List<String>): Int {
+        val smallestCost = part1(input)
+        val map = Grid(input)
+        val start = map.find('S')!!
+        val end = map.find('E')!!
+        val paths = mutableSetOf<Pair<Position, Char>>()
+
+        val ends = smallest.filter { it.key.first == end && it.value == smallestCost }
+        paths.addAll(ends.keys)
+        var prevs = smallestPrev.filter { ends.contains(it.key) }.values.flatten()
+        while (prevs.isNotEmpty() && !prevs.contains(Pair(start, 'E'))) {
+            paths.addAll(prevs)
+            prevs = smallestPrev.filter { prevs.contains(it.key) }.values.flatten()
+        }
+
+        return paths.map { it.first }.toSet().size
     }
 
     println(part1(readInput("aoc2024/Day16_test")))
     println(part1(readInput("aoc2024/Day16_test2")))
     println(part1(readInput("aoc2024/Day16")))
-//    println(part2(readInput("aoc2024/Day16_test")))
-//    println(part2(readInput("aoc2024/Day16")))
+    println(part2(readInput("aoc2024/Day16_test")))
+    println(part2(readInput("aoc2024/Day16_test2")))
+    println(part2(readInput("aoc2024/Day16")))
 }
