3549bc7cf692f23db56c3753c986dd022835e06e
Anita Fronczak <afronczak@netflix.com>
2024-12-17 12:54:49 +0100

    AoC24 17+-

--- src/aoc2024/Day17.kt
+++ src/aoc2024/Day17.kt
@@ -8,18 +8,13 @@ fun main() {
     fun part1Test(): List<Int> {
         val computer = ComputerDay17(729, 0, 0, 0)
         val output = computer.calculate(listOf(0,1,5,4,3,0))
-        println(output)
         return output
     }
 
     fun part1(): List<Int> {
         val computer = ComputerDay17(62769524, 0, 0, 0)
         val output = computer.calculate(listOf(2,4,1,7,7,5,0,3,4,0,1,7,5,5,3,0))
-        println(output)
-        val computer2 = ComputerDay17(62769524, 0, 0, 0)
-        val result = computer2.calculate(output)
-        println(result)
-        return result
+        return output
     }
 
     fun part2(input: List<String>): Int {
