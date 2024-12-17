7fb5d1dc0a4b054b8587d2a1dc20a833ee53f960
Anita Fronczak <afronczak@netflix.com>
2024-12-17 17:25:22 +0100

    AoC24 17+-

--- src/aoc2024/ComputerDay17.kt
+++ src/aoc2024/ComputerDay17.kt
@@ -1,79 +1,79 @@
 package aoc2024
 
-class ComputerDay17(var A: Int, var B: Int, var C: Int, var instructionPointer: Int = 0) {
+class ComputerDay17(var A: Long, var B: Long, var C: Long, var instructionPointer: Int = 0) {
 
-    private fun comboOperand(value: Int): Int {
+    private fun comboOperand(value: Long): Long {
         return when(value) {
-            0 -> 0
-            1 -> 1
-            2 -> 2
-            3 -> 3
-            4 -> A
-            5 -> B
-            6 -> C
+            0L -> 0
+            1L -> 1
+            2L -> 2
+            3L -> 3
+            4L -> A
+            5L -> B
+            6L -> C
             else -> throw IllegalArgumentException()
         }
     }
 
-    private fun adv(operand: Int): Int? {
-        val result = A / (1 shl comboOperand(operand))
+    private fun adv(operand: Long): Long? {
+        val result = A / (1L shl comboOperand(operand).toInt())
         A = result
         instructionPointer += 2
         return null
     }
 
-    private fun bxl(operand: Int): Int? {
+    private fun bxl(operand: Long): Long? {
         val result = B xor operand
         B = result
         instructionPointer += 2
         return null
     }
 
-    private fun bst(operand: Int): Int? {
+    private fun bst(operand: Long): Long? {
         val result = comboOperand(operand) % 8
         B = result
         instructionPointer += 2
         return null
     }
 
-    private fun jnz(operand: Int): Int? {
-        if (A == 0) {
+    private fun jnz(operand: Long): Long? {
+        if (A == 0L) {
             instructionPointer += 2
             return null
         }
 
-        instructionPointer = operand
+        instructionPointer = operand.toInt()
         return null
     }
 
-    private fun bxc(operand: Int): Int? {
+    private fun bxc(operand: Long): Long? {
         val result = B xor C
         B = result
         instructionPointer += 2
         return null
     }
 
-    private fun out(operand: Int): Int {
+    private fun out(operand: Long): Long {
         val result = comboOperand(operand) % 8
         instructionPointer += 2
         return result
     }
 
-    private fun bdv(operand: Int): Int? {
-        val result = A / (1 shl comboOperand(operand))
+    private fun bdv(operand: Long): Long? {
+        val result = A / (1L shl comboOperand(operand).toInt())
         B = result
         instructionPointer += 2
         return null
     }
 
-    private fun cdv(operand: Int): Int? {
-        val result = A / (1 shl comboOperand(operand))
+    private fun cdv(operand: Long): Long? {
+        val result = A / (1L shl comboOperand(operand).toInt())
         C = result
         instructionPointer += 2
         return null
     }
 
-    private fun op(program: List<Int>): Int? {
+    fun nextOp(program: List<Long>): Long? {
         if (instructionPointer + 1 >= program.size)
             return null
 
@@ -81,25 +81,25 @@ class ComputerDay17(var A: Int, var B: Int, var C: Int, var instructionPointer: 
         val operand = program[instructionPointer + 1]
 
         val result = when(opcode) {
-            0 -> adv(operand)
-            1 -> bxl(operand)
-            2 -> bst(operand)
-            3 -> jnz(operand)
-            4 -> bxc(operand)
-            5 -> out(operand)
-            6 -> bdv(operand)
-            7 -> cdv(operand)
+            0L -> adv(operand)
+            1L -> bxl(operand)
+            2L -> bst(operand)
+            3L -> jnz(operand)
+            4L -> bxc(operand)
+            5L -> out(operand)
+            6L -> bdv(operand)
+            7L -> cdv(operand)
             else -> throw IllegalArgumentException()
         }
 
         return result
     }
 
-    fun calculate(program: List<Int>): List<Int> {
+    fun calculate(program: List<Long>): List<Long> {
         instructionPointer = 0
-        var result = mutableListOf<Int>()
+        var result = mutableListOf<Long>()
         while (instructionPointer < program.size) {
-            val opResult = op(program)
+            val opResult = nextOp(program)
             if (opResult != null) {
                 result.add(opResult)
             }
--- src/aoc2024/Day17.kt
+++ src/aoc2024/Day17.kt
@@ -5,19 +5,42 @@ import readInput
 
 fun main() {
 
-    fun part1Test(): List<Int> {
+    fun part1Test(): List<Long> {
         val computer = ComputerDay17(729, 0, 0, 0)
         val output = computer.calculate(listOf(0,1,5,4,3,0))
         return output
     }
 
-    fun part1(): List<Int> {
+    fun part1(): List<Long> {
         val computer = ComputerDay17(62769524, 0, 0, 0)
         val output = computer.calculate(listOf(2,4,1,7,7,5,0,3,4,0,1,7,5,5,3,0))
         return output
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2Test(program: List<Long>): Long {
+        for (A in 0 .. Long.MAX_VALUE) {
+            //println(A)
+            val computer = ComputerDay17(A, 0, 0, 0)
+            var outputIndex = 0
+            var iterationsCount = 0
+            while (iterationsCount < 100000 && outputIndex < program.size) {
+                iterationsCount++
+
+                val cur = computer.nextOp(program)
+
+                if (cur != null) {
+                    if (program[outputIndex] == cur) {
+                        outputIndex++
+                    } else {
+                        break
+                    }
+                }
+            }
+
+            if (outputIndex == program.size) {
+                return A
+            }
+        }
         return 0
     }
 
@@ -25,6 +48,6 @@ fun main() {
 
     println(part1Test())
     println(part1())
-//    println(part2(readInput("aoc2024/Day17_test")))
-//    println(part2(readInput("aoc2024/Day17")))
+    println(part2Test(listOf(0,3,5,4,3,0)))
+    println(part2Test(listOf(2,4,1,7,7,5,0,3,4,0,1,7,5,5,3,0)))
 }
