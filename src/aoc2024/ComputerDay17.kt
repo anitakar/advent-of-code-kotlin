6b375cf9d47b8a390a2ae3b0d07968748f6f402a
Anita Fronczak <afronczak@netflix.com>
2024-12-17 12:45:01 +0100

    AoC24 17--

--- src/aoc2024/ComputerDay17.kt
+++ src/aoc2024/ComputerDay17.kt
@@ -1,7 +1,6 @@
 package aoc2024
 
-class ComputerDay17(var A: Int, var B: Int, var C: Int) {
-    var instructionPointer = 0
+class ComputerDay17(var A: Int, var B: Int, var C: Int, var instructionPointer: Int = 0) {
 
     private fun comboOperand(value: Int): Int {
         return when(value) {
@@ -16,56 +15,95 @@ class ComputerDay17(var A: Int, var B: Int, var C: Int) {
         }
     }
 
-    private fun adv(operand: Int): Int {
+    private fun adv(operand: Int): Int? {
         val result = A / (1 shl comboOperand(operand))
         A = result
-        return result
+        instructionPointer += 2
+        return null
     }
 
-    private fun bxl(operand: Int): Int {
+    private fun bxl(operand: Int): Int? {
         val result = B xor operand
         B = result
-        return result
+        instructionPointer += 2
+        return null
     }
 
-    private fun bst(operand: Int): Int {
+    private fun bst(operand: Int): Int? {
         val result = comboOperand(operand) % 8
         B = result
-        return result
+        instructionPointer += 2
+        return null
     }
 
-    private fun jnz(operand: Int): Int {
-        if (A == 0) return 0
+    private fun jnz(operand: Int): Int? {
+        if (A == 0) {
+            instructionPointer += 2
+            return null
+        }
 
         instructionPointer = operand
-        return 0
+        return null
     }
 
-    private fun bxc(operand: Int): Int {
+    private fun bxc(operand: Int): Int? {
         val result = B xor C
         B = result
-        return result
+        instructionPointer += 2
+        return null
     }
 
     private fun out(operand: Int): Int {
         val result = comboOperand(operand) % 8
+        instructionPointer += 2
         return result
     }
 
-    private fun bdv(operand: Int): Int {
+    private fun bdv(operand: Int): Int? {
         val result = A / (1 shl comboOperand(operand))
         B = result
-        return result
+        instructionPointer += 2
+        return null
     }
 
-    private fun cdv(operand: Int): Int {
+    private fun cdv(operand: Int): Int? {
         val result = A / (1 shl comboOperand(operand))
         C = result
+        instructionPointer += 2
+        return null
+    }
+
+    private fun op(program: List<Int>): Int? {
+        if (instructionPointer + 1 >= program.size)
+            return null
+
+        val opcode = program[instructionPointer]
+        val operand = program[instructionPointer + 1]
+
+        val result = when(opcode) {
+            0 -> adv(operand)
+            1 -> bxl(operand)
+            2 -> bst(operand)
+            3 -> jnz(operand)
+            4 -> bxc(operand)
+            5 -> out(operand)
+            6 -> bdv(operand)
+            7 -> cdv(operand)
+            else -> throw IllegalArgumentException()
+        }
+
         return result
     }
 
     fun calculate(program: List<Int>): List<Int> {
+        instructionPointer = 0
         var result = mutableListOf<Int>()
+        while (instructionPointer < program.size) {
+            val opResult = op(program)
+            if (opResult != null) {
+                result.add(opResult)
+            }
+        }
         return result
     }
 }
--- src/aoc2024/Day17.kt
+++ src/aoc2024/Day17.kt
@@ -5,11 +5,20 @@ import readInput
 
 fun main() {
 
-    fun part1(input: List<String>): List<Int> {
-        val computer = ComputerDay17(729, 0, 0)
+    fun part1Test(): List<Int> {
+        val computer = ComputerDay17(729, 0, 0, 0)
         val output = computer.calculate(listOf(0,1,5,4,3,0))
-        val computer2 = ComputerDay17(729, 0, 0)
+        println(output)
+        return output
+    }
+
+    fun part1(): List<Int> {
+        val computer = ComputerDay17(62769524, 0, 0, 0)
+        val output = computer.calculate(listOf(2,4,1,7,7,5,0,3,4,0,1,7,5,5,3,0))
+        println(output)
+        val computer2 = ComputerDay17(62769524, 0, 0, 0)
         val result = computer2.calculate(output)
+        println(result)
         return result
     }
 
@@ -19,8 +28,8 @@ fun main() {
 
 
 
-    println(part1(readInput("aoc2024/Day17_test")))
-    println(part1(readInput("aoc2024/Day17")))
-    println(part2(readInput("aoc2024/Day17_test")))
-    println(part2(readInput("aoc2024/Day17")))
+    println(part1Test())
+    println(part1())
+//    println(part2(readInput("aoc2024/Day17_test")))
+//    println(part2(readInput("aoc2024/Day17")))
 }
