04967860cf2f1933223550324a3cc3b6adfc39b0
Anita Fronczak <afronczak@netflix.com>
2024-12-19 23:54:47 +0100

    AoC24 19+-

--- src/aoc2024/Day19.kt
+++ src/aoc2024/Day19.kt
@@ -15,24 +15,36 @@ fun main() {
         return Pair(patterns, designs)
     }
 
+    val memoized = mutableMapOf<String, Boolean>()
+
     fun possible(patterns: List<String>, left: String): Boolean {
         if (left.isEmpty()) {
             return true
         }
 
+        val toCheck = mutableListOf<String>()
         for (pattern in patterns) {
-            val toCheck = mutableListOf<String>()
             if (left.startsWith(pattern)) {
                 toCheck.add(left.substring(pattern.length, left.length))
             }
-            if (toCheck.isNotEmpty()) {
-                return toCheck.any { possible(patterns, it) }
+        }
+        if (toCheck.isNotEmpty()) {
+            val result = toCheck.any {
+                if (memoized.contains(it)) {
+                    memoized[it]!!
+                } else {
+                    val toMemoize = possible(patterns, it)
+                    memoized[it] = toMemoize
+                    toMemoize
+                }
             }
+            return result
         }
         return false
     }
 
     fun part1(input: List<String>): Int {
+        memoized.clear()
         val (patterns, designs) = parseInput(input)
         var total = 0
         for (design in designs) {
