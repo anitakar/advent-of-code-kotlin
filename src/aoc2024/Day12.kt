c3e9b29537f9665fa7e7d359b5e0c2af01dfce3c
Anita Fronczak <akarbow@gmail.com>
2024-12-29 16:48:28 +0100

    AoC24 12++

--- src/aoc2024/Day12.kt
+++ src/aoc2024/Day12.kt
@@ -81,138 +81,93 @@ fun main() {
         return false
     }
 
-    fun formsHorizontalLine(position: Position, map: Grid): Boolean {
-        val up = map.up(position)
-        if (up == null) {
-            return true
-        }
-        if (map.get(up) == map.get(position)) {
-            return true
-        }
-        val down = map.down(position)
-        if (down == null) {
-            return true
-        }
-        if (map.get(down) == map.get(position)) {
-            return true
-        }
-        return false
-    }
-
-    fun formsVerticalLine(position: Position, map: Grid): Boolean {
-        val left = map.left(position)
-        if (left == null) {
-            return true
-        }
-        if (map.get(left) == map.get(position)) {
-            return true
-        }
-        val right = map.right(position)
-        if (right == null) {
-            return true
-        }
-        if (map.get(right) == map.get(position)) {
-            return true
-        }
-        return false
-    }
-
-    fun trackHorizontalLine(position: Position, perimeter: MutableList<Position>, map: Grid): MutableList<Position> {
-        val line = mutableListOf<Position>()
-        line.add(position)
-
-        var left = map.left(position)
-        while (left != null && perimeter.contains(left)) {
-            line.add(left)
-            left = map.left(left)
-        }
-
-        var right = map.right(position)
-        while (right != null && perimeter.contains(right)) {
-            line.add(right)
-            right = map.right(right)
-        }
-
-        line.sortBy { -it.y }
-        return line
-    }
-
-    fun trackVerticalLine(position: Position, perimeter: MutableList<Position>, map: Grid): MutableList<Position> {
-        val line = mutableListOf<Position>()
-        line.add(position)
-
-        var up = map.up(position)
-        while (up != null && perimeter.contains(up)) {
-            line.add(up)
-            up = map.up(up)
-        }
-
-        var down = map.down(position)
-        while (down != null && perimeter.contains(down)) {
-            line.add(down)
-            down = map.down(down)
-        }
-
-        line.sortBy { -it.x }
-        return line
-    }
-
     fun part2(input: List<String>): Long {
         val map = Grid(input)
         val areas = findAreas(input)
 
         var cost = 0L
         for (area in areas) {
-            val outsidePerimeterSet = mutableSetOf<Position>()
+            val outsidePerimeterSetUp = mutableSetOf<Position>()
+            val outsidePerimeterSetDown = mutableSetOf<Position>()
+            val outsidePerimeterSetRight = mutableSetOf<Position>()
+            val outsidePerimeterSetLeft = mutableSetOf<Position>()
             for (position in area.positions) {
                 if (onPerimeter(position, map)) {
                     val up = map.up(position)
-                    if (up == null) outsidePerimeterSet.add(Position(-1, position.y))
-                    else if (map.get(up) != map.get(position)) outsidePerimeterSet.add(up)
+                    if (up == null) outsidePerimeterSetUp.add(Position(-1, position.y))
+                    else if (map.get(up) != map.get(position)) outsidePerimeterSetUp.add(up)
 
                     val down = map.down(position)
-                    if (down == null) outsidePerimeterSet.add(Position(position.x + 1, position.y))
-                    else if (map.get(down) != map.get(position)) outsidePerimeterSet.add(down)
+                    if (down == null) outsidePerimeterSetDown.add(Position(position.x + 1, position.y))
+                    else if (map.get(down) != map.get(position)) outsidePerimeterSetDown.add(down)
 
                     val right = map.right(position)
-                    if (right == null) outsidePerimeterSet.add(Position(position.x, position.y + 1))
-                    else if (map.get(right) != map.get(position)) outsidePerimeterSet.add(right)
+                    if (right == null) outsidePerimeterSetRight.add(Position(position.x, position.y + 1))
+                    else if (map.get(right) != map.get(position)) outsidePerimeterSetRight.add(right)
 
                     val left = map.left(position)
-                    if (left == null) outsidePerimeterSet.add(Position(position.x, -1))
-                    else if (map.get(left) != map.get(position)) outsidePerimeterSet.add(left)
+                    if (left == null) outsidePerimeterSetLeft.add(Position(position.x, -1))
+                    else if (map.get(left) != map.get(position)) outsidePerimeterSetLeft.add(left)
                 }
             }
-            var outsidePerimeter = outsidePerimeterSet.toList()
 
+            var linesCount = 0
+            var outsidePerimeter = outsidePerimeterSetUp.toList()
             outsidePerimeter = outsidePerimeter.sortedWith { a, b ->
                 if (a.x != b.x) a.x - b.x else a.y - b.y
             }
-            var horizontalLinesCount = 1
+            if (outsidePerimeter.size > 0)
+                linesCount++
             for (i in 1 until outsidePerimeter.size) {
                 val prev = outsidePerimeter[i-1]
                 val cur = outsidePerimeter[i]
-                if (prev.x != cur.x || prev.y != cur.y - 1) horizontalLinesCount++
+                if (prev.x != cur.x || prev.y != cur.y - 1) linesCount++
             }
 
+            outsidePerimeter = outsidePerimeterSetDown.toList()
+            outsidePerimeter = outsidePerimeter.sortedWith { a, b ->
+                if (a.x != b.x) a.x - b.x else a.y - b.y
+            }
+            if (outsidePerimeter.size > 0)
+                linesCount++
+            for (i in 1 until outsidePerimeter.size) {
+                val prev = outsidePerimeter[i-1]
+                val cur = outsidePerimeter[i]
+                if (prev.x != cur.x || prev.y != cur.y - 1) linesCount++
+            }
+
+            outsidePerimeter = outsidePerimeterSetRight.toList()
             outsidePerimeter = outsidePerimeter.sortedWith { a, b ->
                 if (a.y != b.y) a.y - b.y else a.x - b.x
             }
-            var verticalLinesCount = 1
+            if (outsidePerimeter.size > 0)
+                linesCount++
             for (i in 1 until outsidePerimeter.size) {
                 val prev = outsidePerimeter[i-1]
                 val cur = outsidePerimeter[i]
-                if (prev.y != cur.y || prev.x != cur.x - 1) verticalLinesCount++
+                if (prev.y != cur.y || prev.x != cur.x - 1) linesCount++
             }
 
-            cost += (area.positions.size * (horizontalLinesCount + verticalLinesCount))
+            outsidePerimeter = outsidePerimeterSetLeft.toList()
+            outsidePerimeter = outsidePerimeter.sortedWith { a, b ->
+                if (a.y != b.y) a.y - b.y else a.x - b.x
+            }
+            if (outsidePerimeter.size > 0)
+                linesCount++
+            for (i in 1 until outsidePerimeter.size) {
+                val prev = outsidePerimeter[i-1]
+                val cur = outsidePerimeter[i]
+                if (prev.y != cur.y || prev.x != cur.x - 1) linesCount++
+            }
+
+            cost += (area.positions.size * linesCount)
         }
 
         return cost
     }
 
-//    println(part1(readInput("aoc2024/Day12_test")))
-//    println(part1(readInput("aoc2024/Day12")))
+    println(part1(readInput("aoc2024/Day12_test")))
+    println(part1(readInput("aoc2024/Day12")))
     println(part2(readInput("aoc2024/Day12_test")))
-//    println(part2(readInput("aoc2024/Day12")))
+    println(part2(readInput("aoc2024/Day12")))
 }
